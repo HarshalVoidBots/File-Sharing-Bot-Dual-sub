ff3dffda3aea39b1c90040ae6f939d8d3e1980cb
HarshalVoidBots <harshalborakhade370@gmail.com>
2024-05-28 16:48:35 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='https://t.me/Yuuichi_Sama'>This Person</a>\n○ Source Code : <a href='https://t.me/Yuuichi_Sama'>Click here</a>\n○ Channel : @Animes_VQ\n○ Support Group : @Anime_Chat_Unity</b>",
+            text = f"<b>○ Creator : <a href='https://t.me/Its_Sungjinwoo'>This Person</a>\n○ Source Code : <a href='https://t.me/Its_Sungjinwoo'>Click here</a>\n○ Channel : @Animes_Void\n○ Support Group : @Anime_Void_Chat</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
